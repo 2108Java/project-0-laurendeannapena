ea2ae3fda22a53675a7ab2514e9338d0ee23cae0
Lauren Smith <ldsmith16@liberty.edu>
2021-09-15 15:29:57 -0500

    service interface with descriptions

--- src/main/java/com/revature/service/BankServices.java
+++ src/main/java/com/revature/service/BankServices.java
@@ -1,32 +1,45 @@
 package com.revature.service;
 
+import java.util.ArrayList;
+
 import com.revature.models.Account;
 import com.revature.models.MoneyTransfer;
 import com.revature.models.Transaction;
+import com.revature.models.User;
 
 public interface BankServices {
 	
+	//Check user login credentials
 	public boolean authenticateUser();
 	
 	//Create new User
-	public boolean addUser();
+	public boolean addUser(User newUser);
 	
-	public boolean addNewAccount();
+	//create new checking or savings account
+	public boolean addNewAccount(Account newAccount);
 	
+	//create new joint account
 	public boolean addJointAccount();
 	
+	//view the balance for a specific account
 	public double viewAccountBalance();
 	
+	//make a withdrawal or deposit to an account
 	public boolean changeAccountBalance();
 	
+	//approve or reject pending account registrations
 	public boolean reviewAccountRegistration();
 	
-	public Account viewCustomerAccount();
+	//view a list of a specific customer's accounts
+	public ArrayList<Account> viewCustomerAccount();
 	
+	//create pending money transfer
 	public boolean postMoneyTransfer();
 	
+	//approve or reject pending money transfer
 	public MoneyTransfer reviewMoneyTransfer();
 	
-	public Transaction[] viewTransactions();
+	//view all transactions
+	public ArrayList<Transaction> viewTransactions();
 
 }
