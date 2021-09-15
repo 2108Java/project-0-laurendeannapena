e1e2a49e9bae43e30937527e9b4b6b5c326e09ac
Lauren Smith <ldsmith16@liberty.edu>
2021-09-15 09:03:03 -0500

    service interface v1

--- src/main/java/com/revature/service/BankServices.java
+++ src/main/java/com/revature/service/BankServices.java
@@ -1,8 +1,32 @@
 package com.revature.service;
 
+import com.revature.models.Account;
+import com.revature.models.MoneyTransfer;
+import com.revature.models.Transaction;
+
 public interface BankServices {
+	
+	public boolean authenticateUser();
 	
 	//Create new User
 	public boolean addUser();
+	
+	public boolean addNewAccount();
+	
+	public boolean addJointAccount();
+	
+	public double viewAccountBalance();
+	
+	public boolean changeAccountBalance();
+	
+	public boolean reviewAccountRegistration();
+	
+	public Account viewCustomerAccount();
+	
+	public boolean postMoneyTransfer();
+	
+	public MoneyTransfer reviewMoneyTransfer();
+	
+	public Transaction[] viewTransactions();
 
 }
