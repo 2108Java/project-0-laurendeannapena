75924abcb19a0ac0d8d952aa6a405225ed584d02
Lauren Smith <ldsmith16@liberty.edu>
2021-09-19 19:40:49 -0500

    account model

--- src/main/java/com/revature/models/Account.java
+++ src/main/java/com/revature/models/Account.java
@@ -2,4 +2,92 @@ package com.revature.models;
 
 public class Account {
 
+	private int accountId;
+	private int userIdOne;
+	private int userIdTwo;
+	private String accountType;
+	private double accountBalance;
+	private boolean isApproved;
+	
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Account(int userIdOne, String accountType, double accountBalance, boolean isApproved) {
+		super();
+		this.userIdOne = userIdOne;
+		this.accountType = accountType;
+		this.accountBalance = accountBalance;
+		this.isApproved = isApproved;
+	}
+
+	public Account(int userIdOne, int userIdTwo, String accountType, double accountBalance, boolean isApproved) {
+		super();
+		this.userIdOne = userIdOne;
+		this.userIdTwo = userIdTwo;
+		this.accountType = accountType;
+		this.accountBalance = accountBalance;
+		this.isApproved = isApproved;
+	}
+
+	public Account(int accountId, int userIdOne, int userIdTwo, String accountType, double accountBalance,
+			boolean isApproved) {
+		super();
+		this.accountId = accountId;
+		this.userIdOne = userIdOne;
+		this.userIdTwo = userIdTwo;
+		this.accountType = accountType;
+		this.accountBalance = accountBalance;
+		this.isApproved = isApproved;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public int getUserIdOne() {
+		return userIdOne;
+	}
+
+	public void setUserIdOne(int userIdOne) {
+		this.userIdOne = userIdOne;
+	}
+
+	public int getUserIdTwo() {
+		return userIdTwo;
+	}
+
+	public void setUserIdTwo(int userIdTwo) {
+		this.userIdTwo = userIdTwo;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public boolean isApproved() {
+		return isApproved;
+	}
+
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
+	
 }
